69961da007e1e21e26eae1caad4f7584e6a77d2b
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2021-07-27 22:19:55 -0300

    add alteações na rerceita

---
+++ poema1.py
@@ -0,0 +1,17 @@
+'''
+Bolo de chocolate amargo 
+
+Ingredientes:
+1 barra de chocolate 70% (100g)
+2 ovos
+2 colheres de óleo de coco (pode substituir por outro óleo, 
+manteiga ou margarina)
+1/2 xícara de farinha (usei metade farinha de aveia e a outra 
+metade de farinha de nozes)
+1/2 xícara de açúcar ou adoçante (usei mascavo)
+1 colher de sopa de extrato de baunilha (ou essência)
+1 colher de chá de fermento ou bicarbonato de sódio 
+1 pitada de sal
+'''
+
+
